cab9efc501d11984788ec905fff48d42a888169d
Jaswanth Kothabalireddigari <jaswanthkothabalireddigari@gmail.com>
2026-06-25 00:08:02 +0530

    Completed successfully Assignments 1

---
+++ javascript-traning/basics.js
@@ -0,0 +1,8 @@
+let x=10;
+let y=20;
+let z=x+y;
+console.log(z)
+
+let empName="Jaswanth"
+let empId=12345
+console.log(`the employee name is ${empName} and the employe id is ${empId}`)
---
+++ typescript-traning/Assignments/assignments1.ts
@@ -0,0 +1,40 @@
+let Temp:number=25.5
+console.log(`Temperature of a city in degrees Celsius:${Temp}`)
+let Order:boolean=true
+console.log(`Whether a customer has placed an order:${Order}`)
+let phone_number:number=7729956133
+console.log(`Person's phone number:${phone_number}`)
+let Money:number=10000
+console.log(`Amount of money in a customer's bank account:${Money}`)
+let Email:string="john.doe@example.com"
+console.log(`Person's email address:${Email}`)
+let Cordinates:[number, number]=[37.7749, -122.4194]
+console.log(`Coordinates of a location ${Cordinates}`)
+let Maritial_status:boolean=true
+console.log(`Person's marital status:${Maritial_status}`)
+let Occupation:string="Software Engieer"
+console.log(`Person's occupation:${Occupation}`)
+let Colour:string="Blue"
+console.log(`Person's favourite colour:${Colour}`)
+let Current_Year:number=2026
+console.log(`the Current year is:${Current_Year}`)
+let Followers:number=10000
+console.log(`Number of followers on a social media platform:${Followers}`)
+let Rating:number=8.5
+console.log(`Rating of a movie is:${Rating}`)
+let Blood_Group:string='A'
+console.log(`Person's blood type:${Blood_Group}`)
+let Book:string="To Kill a Mockingbird"
+console.log(`Title of a book:${Book}`)
+let Employees:number=500
+console.log(`Number of employees in a company:${Employees}`)
+let Time:string="02:30 PM"
+console.log(`Time of an event:${Time}`)
+let Country:string="United States"
+console.log(`Name of a country:${Country}`)
+let Eye_Colour:string="Brown"
+console.log(`Person's eye color:${Eye_Colour}`)
+let Birth_Place:string="New York City"
+console.log(`Person's birthplace:${Birth_Place}`)
+let Distance:number=200.5
+console.log(`Distance between two cities:${Distance}`)
